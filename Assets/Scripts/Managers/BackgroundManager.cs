adc540916cbb039963033fb01d6e79f09a0cc2df
adrianqm <adrianqm97@gmail.com>
2024-01-26 01:21:08 +0100

    Adding more assets

--- Assets/Scripts/Managers/BackgroundManager.cs
+++ Assets/Scripts/Managers/BackgroundManager.cs
@@ -9,49 +9,59 @@ using Image = UnityEngine.UI.Image;
 public class BackgroundManager : MonoBehaviour
 {
     public TMP_Dropdown dropdownMenu;
-    public RoundData round;
+    private RoundData _currentRound;
     private RectTransform _rectTransform;
     private Image _image;
 
     public void Awake()
     {
+        RoundChannel.onRoundStarted += OnRoundStarted;
         GameObject imageGo = GameObject.FindGameObjectWithTag("View Image BG");
         _rectTransform = imageGo.GetComponent<RectTransform>();
         _image = imageGo.GetComponent<Image>();
-
-        PopulateList();
     }
 
     public void ActionToCall(int selectedIndex)
     {
         SetBgImage(selectedIndex);
     }
-
-    private void SetBgImage(int selectedIndex)
-    {
-        BackgroundData bgData = round.backgrounds[selectedIndex];
-        _image.sprite = bgData.bg;
-        _rectTransform.transform.localScale = bgData.scale;
-        SetRect(_rectTransform,bgData.left, bgData.top, bgData.right, bgData.bottom );
-    }
     
-    public static void SetRect(RectTransform trs, float left, float top, float right, float bottom)
+    private static void SetRect(RectTransform trs, float left, float top, float right, float bottom)
     {
         trs.offsetMin = new Vector2(left, bottom);
         trs.offsetMax = new Vector2(-right, -top);
     }
 
+    private void SetBgImage(int selectedIndex)
+    {
+        BackgroundData bgData = _currentRound.backgrounds[selectedIndex];
+        _image.sprite = bgData.bg;
+        _rectTransform.transform.localScale = bgData.scale;
+        SetRect(_rectTransform,bgData.left, bgData.top, bgData.right, bgData.bottom );
+    }
+
+    private void OnRoundStarted(RoundController roundController)
+    {
+        _currentRound = roundController.roundData;
+        PopulateList();
+    }
+
     private void PopulateList()
     {
         dropdownMenu.ClearOptions();
-        if (round.backgrounds.Count > 0)
+        if (_currentRound.backgrounds.Count > 0)
         {
             SetBgImage(0);
-            foreach (var background in round.backgrounds)
+            foreach (var background in _currentRound.backgrounds)
             {
                 dropdownMenu.options.Add(new TMP_Dropdown.OptionData(background.title, background.bg));
             }
             dropdownMenu.RefreshShownValue();
         }
+    }
+
+    private void OnDestroy()
+    {
+        RoundChannel.onRoundStarted -= OnRoundStarted;
     }
 }
