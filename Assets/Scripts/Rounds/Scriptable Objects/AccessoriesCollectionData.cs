e3c1d8171c4804e845309ef88e1408c6cca7a6df
mariosep28 <mariosep28@gmail.com>
2024-01-28 17:12:13 +0100

    Final build.

--- Assets/Scripts/Rounds/Scriptable Objects/AccessoriesCollectionData.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "AccessoriesCollection", fileName = "AccessoriesCollection")]
-public class AccessoriesCollectionData : ScriptableObject
-{
-    public List<GameObject> accessoriesList = new List<GameObject>();
-}
